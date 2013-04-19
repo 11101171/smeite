8d7cc6c6b723b91fd969e231f97077986e91896a
zuosanshao <smeite@qq.com>
2013-04-19 11:01:30 +0800

    adjust

--- app/utils/global/Global.scala
+++ app/utils/global/Global.scala
@@ -16,22 +16,24 @@ import play.api.mvc.Results._
 object Global extends GlobalSettings {
 
  /*  When an exception occurs in your application, the onError operation will be called  */
-  /*override def onError(request: RequestHeader, ex: Throwable) = {
+  override def onError(request: RequestHeader, ex: Throwable) = {
     InternalServerError(
       views.html.utils.global.error()
     )
-  }*/
+  }
     /*If the framework doesn’t find an Action for a request, the onHandlerNotFound operation will be called:*/
   override def onHandlerNotFound(request: RequestHeader): Result = {
     NotFound(
-      views.html.utils.global.notFound()
+   //   views.html.utils.global.notFound()
+      views.html.utils.global.error()
     )
   }
 
   /* The onBadRequest operation will be called if a route was found, but it was not possible to bind the request parameters */
   override def onBadRequest(request: RequestHeader, error: String) = {
     BadRequest(
-      views.html.utils.global.badRequest()
+    //  views.html.utils.global.badRequest()
+      views.html.utils.global.error()
     )
 
   }
