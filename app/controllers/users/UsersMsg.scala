e55d04b056df145525e1ea1e982aee48053e5b50
zuosanshao <zuosanshao@qq.com>
2013-05-19 23:56:55 +0800

    adjust

--- app/controllers/users/UsersMsg.scala
+++ app/controllers/users/UsersMsg.scala
@@ -13,20 +13,17 @@ object UsersMsg extends Controller {
        def system =Users.UserAction{  user => implicit request =>
         Ok(views.html.users.msg.system(user))
        }
-  /*评论我的*/
+  /*评论 回复 我的*/
       def  comment =Users.UserAction{  user => implicit request =>
         Ok(views.html.users.msg.comment(user))
       }
-  /*回复我的*/
-  def  reply =Users.UserAction{    user => implicit request =>
-    Ok(views.html.users.msg.reply(user))
+  /*喜欢我的主题 宝贝*/
+  def love = Users.UserAction{    user => implicit request =>
+    Ok(views.html.users.msg.love(user))
   }
   /*@我的*/
   def  at =Users.UserAction{     user => implicit request =>
     Ok(views.html.users.msg.at(user))
   }
-  /*我的求鉴定*/
-  def  appraisal =Users.UserAction{  user => implicit request =>
-    Ok(views.html.users.msg.appraisal(user))
-  }
+
 }
