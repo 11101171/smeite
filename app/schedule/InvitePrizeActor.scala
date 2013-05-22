644e21ec452a3d18631d3cdd7c08d2c4394731a2
zuosanshao <smeite@qq.com>
2013-05-22 09:22:20 +0800

    adjust

--- app/schedule/InvitePrizeActor.scala
+++ app/schedule/InvitePrizeActor.scala
@@ -15,22 +15,32 @@ import models.user.dao.UserDao
 class InvitePrizeActor extends  Actor{
 
   def invitePrize(days:Int) ={
+    println("invite prize schedule start *****")
      val  possibleInvitePrizes= UserDao.findPossibleInvitePrizes(99,days)
      for((uid,credits,inviteId) <- possibleInvitePrizes){
        if(credits <1000){
          val invitePrize = UserDao.findUserInvitePrize(inviteId,uid,200)
          if(invitePrize.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,200)
+
+         val invitePrize2 = UserDao.findUserInvitePrize(inviteId,uid,300)
+         if(invitePrize2.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,300)
+
        } else if(credits >=5000) {
+         val invitePrize1 = UserDao.findUserInvitePrize(inviteId,uid,200)
+         if(invitePrize1.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,200)
+
+         val invitePrize2 = UserDao.findUserInvitePrize(inviteId,uid,300)
+         if(invitePrize2.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,300)
+
          val invitePrize = UserDao.findUserInvitePrize(inviteId,uid,500)
          if(invitePrize.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,500)
+
        } else {
          val invitePrize = UserDao.findUserInvitePrize(inviteId,uid,300)
          if(invitePrize.isEmpty)UserDao.addUserInvitePrize(inviteId,uid,credits,300)
        }
-
-
      }
-
+    println("invite prize schedule end !")
   }
   def receive = {
     case "start" => invitePrize(1)
--- app/utils/global/Global.scala
+++ app/utils/global/Global.scala
@@ -15,7 +15,7 @@ import play.api.mvc.Results._
 import scala.concurrent.duration._
 import play.api.libs.concurrent.Akka
 import akka.actor.Props
-import schedule.{InvitePrizeActor}
+import schedule.{TestActor, InvitePrizeActor}
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
@@ -23,8 +23,14 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 object Global extends GlobalSettings {
 
   override def onStart(app: Application) {
-    val invitePrizeActor = Akka.system.actorOf(Props[InvitePrizeActor], name = "invitePrizeActor")
-    Akka.system.scheduler.schedule(1 seconds, 1 days, invitePrizeActor, "start")
+   val invitePrizeSchedule = Play.maybeApplication.flatMap(_.configuration.getString("invitePrize.schedule")).getOrElse("off")
+     if(invitePrizeSchedule=="on"){
+    //   val testActor = Akka.system.actorOf(Props[TestActor], name = "testActor")
+  //     Akka.system.scheduler.schedule(1 seconds, 60 seconds, testActor, "start")
+       val invitePrizeActor = Akka.system.actorOf(Props[InvitePrizeActor], name = "invitePrizeActor")
+       Akka.system.scheduler.schedule(1 seconds, 30 seconds, invitePrizeActor, "start")
+     }
+
   }
 //  When an exception occurs in your application, the onError operation will be called
  override def onError(request: RequestHeader, ex: Throwable) = {
