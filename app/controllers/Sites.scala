cf2fcb2d22f53331b4d0aba3cfbaae1c6a25b846
zuosanshao <smeite@qq.com>
2013-06-21 16:29:56 +0800

    adjust site

--- app/controllers/Sites.scala
+++ app/controllers/Sites.scala
@@ -5,13 +5,23 @@ import controllers.users.Users
 
 
 object Sites extends Controller {
-    /* 小站首页 */
-  def view(id:Long) = Users.UserAction { user => implicit request =>
-   Ok("todo")
+  /* 小站编辑创建 */
+  def editSite(id:Long) = Users.UserAction { user => implicit request =>
+    Ok(views.html.sites.editSite(user))
+  }
+
+  /* 小站  */
+  def site(id:Long) = Users.UserAction { user => implicit request =>
+    Ok(views.html.sites.site(user))
+  }
+
+  /* 小站 帖子 编辑创建 */
+  def editPost(id:Long) = Users.UserAction { user => implicit request =>
+    Ok(views.html.sites.editPost(user))
   }
   /* 小站帖子 */
   def post(sid:Long,pid:Long)= Users.UserAction { user => implicit request =>
-      Ok("todo")
+    Ok(views.html.sites.post(user))
   }
 
 }
