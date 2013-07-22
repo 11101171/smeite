90b73384fd6d539335bc45dd7839c102f1f7be95
zuosanshao <smeite@qq.com>
2013-07-22 15:51:21 +0800

    adjust before master merge

--- app/models/Cate.scala
+++ app/models/Cate.scala
@@ -8,7 +8,7 @@ package models
  * ***********************
  * description:网站的主题分类，适用于主题分类、标签组分类
  * 主要分为5大类 1、健康美食 2、地理特产 3、滋补保健 4、居家生活 5 好玩意   6 其他
- * 新分类 1、零食  2 食材 3 居家
+ * 新分类 1、零食  2 食材 3 居家   4 其他
  * 由于主题分类比较明确、固定，因此，可以写死在程序中，以替代themeCategory
  */
 
@@ -17,4 +17,5 @@ object Cate extends Enumeration {
   val  materialFood = Value("食材")
   val  liveHome =Value("居家")
   val Others =Value("其他")
+
 }
